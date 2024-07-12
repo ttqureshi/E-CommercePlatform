b20346760cc4ea7e45b10e2358d1a37e91aeef3e
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-07-12 17:03:53 +0500

    feat: Add Pagination to product listing

--- eCommercePlatform/products/views.py
+++ eCommercePlatform/products/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Product
-
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def products_listing_view(request):
     products = Product.objects.all()
-    return render(request, "products/products_listing.html", {"products": products})
+    p = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = p.page(1)
+    except EmptyPage:
+        page_obj = p.page(p.num_pages)
+
+    return render(request, "products/products_listing.html", {"page_obj": page_obj})
 
 
 def product_detail_view(request, product_id):
